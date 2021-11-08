812f2a109becee7368bbc764b0c399bca0ffd3d1
Phalakorn223 <phalakorn061@gmail.com>
2021-11-08 13:43:58 +0700

    initial commit

--- bin/dartapp204.dart
+++ bin/dartapp204.dart
@@ -1,5 +1,12 @@
 void main() {
   final msgHello = 'Hello';
+  final String name = "Jhon Travota";
+
+  const provinde = "trat";
+  const String countries = "Thailand";
 
   print(msgHello);
+  print(name);
+  print(provinde);
+  print(countries);
 }
